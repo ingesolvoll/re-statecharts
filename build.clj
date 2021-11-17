becca777eeb0b955f11b899ec5c8b21e97b8e7d0
ingesolvoll <inge.solvoll@gmail.com>
2021-11-17 11:39:11 +0100

    CircleCI setup

--- build.clj
+++ build.clj
@@ -1,18 +1,35 @@
 (ns build
-  (:require [clojure.tools.build.api :as b]
-            [org.corfield.build :as bb]))
+  (:require
+   [clojure.string :as str]
+   [org.corfield.build :as bb]))
 
 (def lib 're-statecharts/re-statecharts)
 (def version "0.0.1")
 
 (defn install [opts]
-      (-> opts
-          (assoc :lib lib :version version)
-          (bb/jar)
-          (bb/install)))
+  (-> opts
+      (assoc :lib lib :version version)
+      (bb/jar)
+      (bb/install)))
 
-(defn deploy [opts]
-      (-> opts
-          (assoc :lib lib :version version)
-          (bb/jar)
-          (bb/deploy)))
+(def release-marker "Release-")
+
+(defn extract-version [tag]
+  (str/replace-first tag release-marker ""))
+
+(defn maybe-deploy [opts]
+  (if-let [tag (System/getenv "CIRCLE_TAG")]
+    (println "Found tag " tag)
+    (if (re-find (re-pattern release-marker) tag)
+      (do
+        (println "Releasing to clojars...")
+        (-> opts
+            (assoc :lib lib :version (extract-version tag))
+            (bb/jar)
+            (bb/deploy)))
+      (do
+        (println "Tag is not a release tag, skipping deploy")
+        opts))
+    (do
+      (println "No tag found, skipping deploy")
+      opts)))
